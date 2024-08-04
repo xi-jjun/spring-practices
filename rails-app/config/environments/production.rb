d6dbe0214f50c480fdf83c1491a551b7899cfe6f
Kim Jae Jun <rlawowns97@naver.com>
2024-08-04 16:22:29 +0900

    rspec-report-redis : add redis config on rails-app project
    - for test on Github actions

--- rails-app/config/environments/production.rb
+++ rails-app/config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_URL') { 'redis://127.0.0.1:6380/' }, expires_in: 5.minutes }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
