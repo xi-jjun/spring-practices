96209373e9102a654c45a5f1af0b0cb1ea189501
Kim Jae Jun <rlawowns97@naver.com>
2024-08-04 16:23:45 +0900

    rspec-report-redis : add sample redis cache store rspec test

--- rails-app/app/models/product.rb
+++ rails-app/app/models/product.rb
@@ -1,7 +1,20 @@
 class Product < ApplicationRecord
+  after_save :refresh_cache
+
   module Enable
     OFF = 0
     ON = 1
+  end
+
+  module CacheInfo
+    FETCH_HASH_BY_ID = {
+      key: 'product_h_%{product_id}'
+    }
+  end
+
+  def self.fetch_hash_by_id(product_id)
+    cache_key = cache_key_for_fetch_by_id(product_id)
+    Rails.cache.read(cache_key).to_h
   end
 
   def set_enable
@@ -20,5 +33,17 @@ class Product < ApplicationRecord
     return false unless stock > 0
 
     true
+  end
+
+  private
+
+  def refresh_cache
+    cache_key = self.class.cache_key_for_fetch_by_id(id)
+    product_hash = self.attributes.transform_keys(&:to_sym)
+    Rails.cache.write(cache_key, product_hash)
+  end
+
+  def self.cache_key_for_fetch_by_id(product_id)
+    CacheInfo::FETCH_HASH_BY_ID[:key] % { product_id: product_id }
   end
 end
--- rails-app/spec/models/product_spec.rb
+++ rails-app/spec/models/product_spec.rb
@@ -51,4 +51,42 @@ RSpec.describe Product, type: :model do
       end
     end
   end
+
+  describe '.refresh_cache' do
+    context '상품이 최초 생성됐을 때' do
+      it '캐시가 자동으로 생성된다.' do
+        # 상품 최초 생성
+        product = Product.create!(name: '상품1',
+                                  code: 'PRODUCT_TEST1',
+                                  price: 1000,
+                                  stock: 10,
+                                  enable: Product::Enable::ON)
+
+        product_hash = Product.fetch_hash_by_id(product.id)
+        expect(product_hash[:id]).to eq(product.id)
+        expect(product_hash[:name]).to eq(product.name)
+        expect(product_hash[:price]).to eq(product.price)
+      end
+    end
+
+    context '상품이 수정됐을 때' do
+      it '캐시가 자동으로 업데이트된다.' do
+        # 기존 상품
+        product = Product.create!(name: '기존 상품1',
+                                  code: 'PRODUCT_TEST1',
+                                  price: 1000,
+                                  stock: 10,
+                                  enable: Product::Enable::ON)
+
+        product_hash = Product.fetch_hash_by_id(product.id)
+        expect(product_hash[:price]).to eq(1000)
+        # 상품 가격 업데이트
+        product.update!(price: 3000)
+
+        product_hash = Product.fetch_hash_by_id(product.id)
+        expect(product_hash[:name]).to eq('기존 상품1')
+        expect(product_hash[:price]).to eq(3000)
+      end
+    end
+  end
 end
