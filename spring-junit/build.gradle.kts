772ecb1566713b01fd0fc440772411d72c21f564
Kim Jae Jun <rlawowns97@naver.com>
2024-12-22 23:48:03 +0900

    spring-test-framework : add a config for generating a junit test result file

--- spring-junit/build.gradle.kts
+++ spring-junit/build.gradle.kts
@@ -35,4 +35,32 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    reports {
+        junitXml.required.set(true) // XML 리포트 활성화
+        junitXml.outputLocation.set(file("$buildDir/test-results/cases"))
+    }
+
+    // 테스트 케이스별로 하나의 파일이 만들어 짐.
+    // 따라서 하나의 파일로 모으기 위한 task를 추가함.
+    finalizedBy("mergeTestResults")
+}
+
+tasks.register("mergeTestResults") {
+    doLast {
+        val resultDir = file("$buildDir/test-results/cases")
+        val mergedFile = file("$buildDir/test-results/merged-test-results.xml")
+
+        val testFiles = resultDir.listFiles { file -> file.name.endsWith(".xml") } ?: return@doLast
+        mergedFile.writeText("<testsuites>\n")
+
+        testFiles.forEach { file ->
+            var content = file.readText()
+            content = content.replaceFirst("""<\?xml.*\?>""".toRegex(), "") // XML 선언 제거
+            content = content.replaceFirst("<testsuites>", "").replaceFirst("</testsuites>", "") // testsuites 태그 제거
+            mergedFile.appendText(content)
+        }
+
+        mergedFile.appendText("</testsuites>")
+        println("Merged test results saved to: $mergedFile")
+    }
 }
