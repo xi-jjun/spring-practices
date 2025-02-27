6bb55f8823366ee49c41b2b1bbf5b3b5ed52fb13
Kim Jae Jun <rlawowns97@naver.com>
2025-02-27 22:47:10 +0900

    multi-module : init project

---
+++ multi-module-tutorial/src/main/kotlin/me/practice/multimoduletutorial/MultiModuleTutorialApplication.kt
@@ -0,0 +1,11 @@
+package me.practice.multimoduletutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MultiModuleTutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<MultiModuleTutorialApplication>(*args)
+}
---
+++ multi-module-tutorial/src/test/kotlin/me/practice/multimoduletutorial/MultiModuleTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.practice.multimoduletutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MultiModuleTutorialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
