10658dce877ed73bb98f07485c88a465a778107d
Kim Jae Jun <rlawowns97@naver.com>
2025-04-13 03:37:34 +0900

    multi-module : infra 모듈에 간단한 repository 코드 추가

---
+++ multi-module-tutorial/infra/db/tutorial-postgresql/src/main/kotlin/me/practice/tutorialpostgresql/repository/EchoBoardJpaRepository.kt
@@ -0,0 +1,7 @@
+package me.practice.tutorialpostgresql.repository
+
+import me.practice.tutorialpostgresql.entity.EchoBoardEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface EchoBoardJpaRepository : JpaRepository<EchoBoardEntity, Long> {
+}
