ce752aad67d50c42be962bb138e8d8201619c1ef
Kim Jae Jun <rlawowns97@naver.com>
2025-04-21 02:53:48 +0900

    multi-module : web api 모듈에서 infra 모듈을 사용하기 위해 추가적인 컴포넌트 스캔 코드 추가 (잘못된 패키지명 사용으로 인한 불필요한 코드 추가..)
    - 수정하고 커밋하지 않은 이유는, 이런 시행착오를 했음을 남기기 위함.

--- multi-module-tutorial/infra/db/tutorial-postgresql/src/main/kotlin/me/practice/tutorialpostgresql/repository/EchoBoardRepositoryImpl.kt
+++ multi-module-tutorial/infra/db/tutorial-postgresql/src/main/kotlin/me/practice/tutorialpostgresql/repository/EchoBoardRepositoryImpl.kt
@@ -3,8 +3,12 @@ package me.practice.tutorialpostgresql.repository
 import me.practice.tutorialdomain.domain.EchoBoard
 import me.practice.tutorialdomain.repository.IEchoBoardRepository
 import me.practice.tutorialpostgresql.entity.EchoBoardEntity
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.stereotype.Repository
 
+// 최초에 패키지 구성을 바보같이 해놔서, 실행모듈 입장에서 패키지 구조를 통해 자동으로 스캔이 불가능.
+// 따라서 직접 스캔할 패키지명을 임시로 명시하여 실행되도록 함
+@EnableJpaRepositories("me.practice.tutorialpostgresql.repository")
 @Repository
 class EchoBoardRepositoryImpl(
     private val echoBoardJpaRepository: EchoBoardJpaRepository
--- multi-module-tutorial/tutorial-web/src/main/kotlin/me/practice/tutorialweb/TutorialWebApplication.kt
+++ multi-module-tutorial/tutorial-web/src/main/kotlin/me/practice/tutorialweb/TutorialWebApplication.kt
@@ -1,8 +1,14 @@
 package me.practice.tutorialweb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
+// 최초에 패키지 구성을 바보같이 해놔서, 실행모듈 입장에서 패키지 구조를 통해 자동으로 스캔이 불가능.
+// 따라서 직접 스캔할 패키지명을 임시로 명시하여 실행되도록 함
+@ComponentScan("me.practice") // 가장 상위 패키지부터 전부 포함
+@EntityScan("me.practice") // entity 스캔
 @SpringBootApplication
 class TutorialWebApplication
 
